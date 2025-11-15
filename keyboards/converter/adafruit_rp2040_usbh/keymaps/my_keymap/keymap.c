66986420c38226a093930c63ae5f2728f667043a
angels38me <angels38me@gmail.com>
2025-11-15 12:02:14 -0700

    Update keymap.c

--- keyboards/converter/adafruit_rp2040_usbh/keymaps/my_keymap/keymap.c
+++ keyboards/converter/adafruit_rp2040_usbh/keymaps/my_keymap/keymap.c
@@ -19,14 +19,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_1] = LAYOUT_fullsize_ansi(
         TO(0),            KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,     DUP_LINE,  SEL_LINE, QK_LEADER,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DK_CLS,  DK_NEW,  KC_NO,      KC_NO,     DEL_LINE, KC_NO,      KC_CALC,         KC_NO,      KC_ACL1,     KC_ACL0,
-        KC_NO,   KC_NO,   S(KC_TAB), KC_UP, KC_TAB,  KC_HOME, KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   DK_LFT,  DK_RHT,  KC_NO,      KC_NO,     KC_NO,    KC_NO,      KC_MS_WH_UP,     KC_MS_UP,   KC_NO,       KC_ACL1,
-        KC_LCTL, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_NO,   KC_4,    KC_5,    KC_6,    KC_BSPC, KC_ENT,           KC_NO,                                       KC_MS_LEFT,      KC_MS_BTN1, KC_MS_RIGHT,
-        KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,            KC_NO,                 KC_MYCM,              KC_MS_WH_DOWN,   KC_MS_DOWN, KC_MS_BTN2,  KC_NO,
-        KC_NO,   KC_NO,   SEL_LINE,                           KC_0,                               KC_0,    KC_DOT,  KC_NO,   KC_MAIL,    KC_WBAK,   KC_WHOM,  KC_WFWD,    KC_NO,           KC_NO
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DK_CLS,  DK_NEW,  HYPER,      KC_NO,     DEL_LINE, KC_NO,            KC_CALC,         KC_NO,      KC_ACL1,     KC_ACL0,
+        KC_NO,   KC_NO,   S(KC_TAB), KC_UP, KC_TAB,  KC_HOME, KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   DK_LFT,  DK_RHT,  ALT_ESC,    H1,        H2,       H3,               KC_MS_WH_UP,     KC_MS_UP,   KC_NO,       KC_ACL1,
+        KC_LCTL, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_NO,   KC_4,    KC_5,    KC_6,    KC_BSPC, KC_ENT,           KC_NO,                                             KC_MS_LEFT,      KC_MS_BTN1, KC_MS_RIGHT,
+        KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,            KC_NO,                 KC_MYCM,                    KC_MS_WH_DOWN,   KC_MS_DOWN, KC_MS_BTN2,  KC_NO,
+        KC_NO,   KC_NO,   SEL_LINE,                           KC_0,                               KC_0,    KC_DOT,  KC_NO,   KC_MAIL,    KC_WBAK,   KC_WHOM,  KC_WFWD,          KC_NO,           KC_NO
     ),
 
-    [LAYER_2] = LAYOUT_fullsize_ansi(
+    [LAYER_2] = LAYOUT_fullsize_ansi(               
         TO(0),            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, QK_LEADER,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_7] = LAYOUT_fullsize_ansi(
-        TO(0),            CD_FIRMWARE,  MAKE_KEYMAP, MAKE_CODER, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    HIGH_CONTRAST, KC_TRNS, QK_LEADER,
+        TO(0),            CD_FIRMWARE,  MAKE_KEYMAP, MAKE_CODER, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    HIGH_CONTRAST, HRM_TOG, QK_LEADER,
         KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                                  KC_TRNS, KC_TRNS, KC_TRNS,
